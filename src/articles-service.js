2020f7550c713eb195070df6e08cc75700f888c1
matrayu <northridge614@gmail.com>
2019-05-25 13:47:38 -0700

    Update articles-service.js
    Updated services file to Blogful app. Includes comments.

--- src/articles-service.js
+++ src/articles-service.js
@@ -1,7 +1,40 @@
 const ArticlesService = {
+    //use this Knex instance to query the blogful_articles table
     getAllArticles(knex) {
         return knex.select('*').from('blogful_articles')
-    }
+    },
+    insertArticle(knex, newArticle) {
+        return knex
+            .insert(newArticle)
+            .into('blogful_articles')
+            //.returning() method we can specify which columns we want to select.
+            .returning('*')
+            //pull out the object from the array we are accessing
+            //we've inserted 1 object to an empty array
+            .then(rows => {
+                return rows[0];
+            });
+    },
+    getById(knex, id) {
+        /* return Promise.resolve({}) */
+        
+        //select all articles where the id is the id passed in retriving the first record
+        return knex.from('blogful_articles')
+            .select('*')
+            .where('id', id)
+            .first()
+    },
+    deleteArticle(knex, id) {
+        /* return Promise.resolve({}) */
+        return knex('blogful_articles')
+            .where({ id })
+            .delete()
+    },
+    updateArticles(knex, id, newArticleFields) {
+        return knex('blogful_articles')
+            .where({ id })
+            .update(newArticleFields)
+    },
 }
 
 module.exports = ArticlesService;
